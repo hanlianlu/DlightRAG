33f71a12c067ca5c32d4275baf56c57b7ec2df1c
hlyu <hanlian.lyu@volvocars.com>
2026-03-18 22:49:06 +0100

    minor fix pg init

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,7 @@
 -- Auto-create required extensions for dlightrag
 CREATE EXTENSION IF NOT EXISTS vector;
 CREATE EXTENSION IF NOT EXISTS age;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
 -- AGE requires the ag_catalog schema on search_path
 SET search_path = ag_catalog, "$user", public;
